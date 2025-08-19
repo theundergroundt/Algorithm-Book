63c2161ce5be5da2da383982e0f9f195039f61cb
sangji <139340193+theundergroundt@users.noreply.github.com>
2025-08-19 13:41:52 +0900

    [D2] Title: 나무 높이, Time: 106 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -3,53 +3,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//15:44
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for(int test = 1; test<=t; test++){
-            int n = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int[] arr = new int[n];
-            int maxnum = Integer.MIN_VALUE;
-            int total=0;
-            for(int i=0; i<n; i++){
-                arr[i] = Integer.parseInt(st.nextToken());
-                total+=arr[i];
-                if(maxnum<arr[i]) maxnum = arr[i];
-            }
-            total-=maxnum;
-            int number = (maxnum*(n-1) - (total));
-
-            if(number == 0) {
-                System.out.println("#"+test+" "+0);
-                continue;
-            }
-            //
-            int odd = 0;
-            for(int height : arr) {
-                int diff = maxnum - height;
-                if (diff > 0) {
-                    odd += (diff % 2);
-                }
-            }
-
-            int count = number/3;
-            count*=2;
-            number = number%3;
-            if(number>0) {
-                if(number == 1){
-                    count+=1;
-                }
-                else if(number == 2){
-                    count+=2;
-                }
-            }
-            // 1 3 5 7...
-            int daysOdd = odd * 2 - 1;
-            int result = Math.max(count, daysOdd);
-            System.out.println("#"+test+" "+result);
-        }
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for(int test =1; test<= t; test++) {
+			int n = Integer.parseInt(br.readLine());
+			int[] arr = new int[n];
+			int maxnum = Integer.MIN_VALUE;
+			int total=0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i=0; i<n; i++) {
+				int a = Integer.parseInt(st.nextToken());
+				arr[i] = a;
+				total+=a;
+				if(maxnum<a) maxnum = a;
+			}
+			total = maxnum*(n-1) - (total-maxnum);
+			// 홀수 인 애들
+			int cnt = 0;
+			for(int i=0; i<n; i++) {
+				if((maxnum-arr[i])%2 == 1) cnt++;
+			}
+			cnt = 2*cnt -1;
+			
+			if(total == 0) {
+				System.out.println("#"+test+" "+0);
+				continue;
+			}
+			
+			int a = total/3;
+			a*=2;
+			int b = total%3;
+			if(b>0) {
+				if( b == 1) a +=1;
+				else a = a+=2;
+			}
+			
+			
+			if(cnt<a)System.out.println("#"+test+" "+a);
+			else System.out.println("#"+test+" "+cnt);
+			
+		}
+	}
 }
+
+/*
+5
+2
+5 5
+2
+4 2
+2
+3 4
+4
+2 3 10 5
+4
+1 2 3 4
+*/
