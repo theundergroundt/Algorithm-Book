9cd65caa94838930c38e87cc479d81428c56f1da
sangji <139340193+theundergroundt@users.noreply.github.com>
2025-08-20 09:34:34 +0900

    [D3] Title: 상호의 배틀필드, Time: 161 ms, Memory: 31,348 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -4,96 +4,88 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for(int test=1; test<=t; test++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int h = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
-            Character[][] matrix = new Character[h][w];
-            int x = -1, y = -1;
-            for(int i=0; i<h; i++) {
-                String a = br.readLine();
-                for(int j=0; j<w; j++) {
-                    Character c = a.charAt(j);
-                    matrix[i][j] = c;
-                    // 어차피 하나
-                    if (matrix[i][j] == '>' || matrix[i][j] == '<' || matrix[i][j] == '^' || matrix[i][j] == 'v') {
-                        x = i;
-                        y = j;
-                    }
-                }
-            }
-            int n = Integer.parseInt(br.readLine());
-            String commands = br.readLine();
-            
-            for (char command : commands.toCharArray()) {
-                // shoot
-                if (command == 'S') {
-                  
-                    char direction = matrix[x][y];
-                    int bulletX = x;
-                    int bulletY = y;
-
-                    while (true) {
-                        if (direction == '^') bulletX--;
-                        else if (direction == 'v') bulletX++;
-                        else if (direction == '<') bulletY--;
-                        else if (direction == '>') bulletY++;
-                        
-                        if (bulletX < 0 || bulletX >= h || bulletY < 0 || bulletY >= w) {
-                            break;
-                        }
-                        // 벽돌
-                        if (matrix[bulletX][bulletY] == '*') {
-                            matrix[bulletX][bulletY] = '.';
-                            break;
-                        }
-                        // 강철
-                        if (matrix[bulletX][bulletY] == '#') {
-                            break;
-                        }
-                    }
-                } else {
-                    
-                    int nextX = x;
-                    int nextY = y;
-                    char nextDir = ' ';
-
-                    if (command == 'U') {
-                        nextDir = '^';
-                        nextX--;
-                    } else if (command == 'D') {
-                        nextDir = 'v';
-                        nextX++;
-                    } else if (command == 'L') {
-                        nextDir = '<';
-                        nextY--;
-                    } else if (command == 'R') {
-                        nextDir = '>';
-                        nextY++;
-                    }
-
-                    // 방향
-                    matrix[x][y] = nextDir;
-                    if (nextX >= 0 && nextX < h && nextY >= 0 && nextY < w && matrix[nextX][nextY] == '.') {
-                        matrix[nextX][nextY] = matrix[x][y];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for(int test=1; test<=t; test++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int h = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int x=0, y=0;
+			char[][] matrix = new char[h][w];
+			for(int i=0; i<h; i++) {
+				String a = br.readLine();
+				for(int j=0; j<w; j++) {
+					char c = a.charAt(j);
+					matrix[i][j] = c;
+					if(c == '>'|| c == '<' || c == '^' || c == 'v') {
+						x = i;
+						y = j;
+					}
+				}
+			}
+			int n = Integer.parseInt(br.readLine());
+			String order = br.readLine();
+			System.out.print("#"+test+" ");
+			for(char c : order.toCharArray()) {
+				// shoot
+				if(c == 'S') {
+					char dir = matrix[x][y];
+					int tmpX = x;
+					int tmpY = y;
+					while(true) {
+						if(dir == '>') tmpY++;
+						else if(dir == '<') tmpY--;
+						else if(dir == '^') tmpX--;
+						else if(dir == 'v') tmpX++;
+						
+						if(tmpX<0 || tmpX>=h || tmpY<0 || tmpY>=w) break;
+						char cur = matrix[tmpX][tmpY];
+						// 강철
+						if(cur == '#') break;
+						// 벽돌
+						if(cur == '*') {
+							matrix[tmpX][tmpY] = '.';
+							break;
+						}
+						
+					}					
+				}
+				else {
+					char nextdir = matrix[x][y];
+					int nextX = x;
+					int nextY = y;
+					
+					if(c == 'U') {
+						nextX--;
+						nextdir = '^';
+					}else if(c == 'D') {
+						nextX++;
+						nextdir = 'v';
+					}else if(c == 'L') {
+						nextY--;
+						nextdir = '<';
+					}else if(c == 'R') {
+						nextY++;
+						nextdir = '>';
+					}
+					
+					
+					matrix[x][y] = nextdir;
+					if(nextX >= 0 && nextX < h && nextY >= 0 && nextY < w && matrix[nextX][nextY] == '.') {
+						matrix[nextX][nextY] = matrix[x][y];
                         matrix[x][y] = '.';
-                        x = nextX;
-                        y = nextY;
-                    }
-                }
-
-            }
-            System.out.print("#" + test + " ");
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    System.out.print(matrix[i][j]);
-                }
-                System.out.println();
-            }
-        }
-    }
+						x = nextX;
+						y = nextY;
+					}
+				}				
+			}
+			for(int i=0; i<h; i++) {
+				for(int j=0; j<w; j++) {
+					System.out.print(matrix[i][j]);
+				}
+				System.out.println("");
+			}
+		}			
+	}
 }
