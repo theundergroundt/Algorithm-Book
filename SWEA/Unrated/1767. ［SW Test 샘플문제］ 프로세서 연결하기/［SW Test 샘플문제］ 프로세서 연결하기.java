a76d6291eac2fb644c55e52f567760dbcec0cc8e
sangji <139340193+theundergroundt@users.noreply.github.com>
2025-08-31 19:36:12 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 164 ms, Memory: 29,680 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -6,91 +6,80 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static class Pair{
-		int x, y;
-		public Pair(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-	static int[] dr = {-1, 1, 0, 0}; // 상, 하, 좌, 우
-    static int[] dc = {0, 0, -1, 1};
-	static List<Pair> li;
-	static int[][] map;
-	static int maxnum,  minwireLength, n;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
-		for(int test = 1;test<=t; test++) {
-			n = Integer.parseInt(br.readLine());
-			map = new int[n][n];
-			li = new ArrayList<>();
-			for(int i=0; i<n; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				for(int j=0; j<n;j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());	
-					if(map[i][j] == 1) {
-						if(i==0 || i==n-1||j==0 || j==n-1) continue;
-						li.add(new Pair(i, j));
-					}
-				}
-			}
-			minwireLength = Integer.MAX_VALUE;
-			maxnum = 0;
-			dfs(0,0,0);
-			System.out.println("#"+test+" "+minwireLength);
-		}		
-	}
-	public static void dfs(int index, int num, int sum) {
-		
-		if(index == li.size()) {
-			if(num>maxnum) {
-				maxnum = num;
-				minwireLength = sum;
-			}else if(num == maxnum) {
-				minwireLength = Math.min(minwireLength, sum);
-			}
-			return;
-		}
-		
-		Pair cur = li.get(index);
-		int r = cur.x;
-		int c = cur.y;
-		for(int d=0; d<4; d++) {
-			if(isconnectable(r,c,d)) {
-				// 전선 2로 놓기
-				int len = setwire(r,c,d,2);
-				dfs(index+1, num+1, sum+len);
-				// 전선 지우기
-				setwire(r,c,d,0);
-			}
-		}
-		dfs(index+1, num, sum);
-	}
-	// 전선 놓을 수 있는지 확인
-	public static boolean isconnectable(int r, int c, int d) {
-		int nr = r;
-		int nc = c;
-		while(true) {
-			nr+=dr[d];
-			nc+=dc[d];
-			if(nr<0 || nr>=n || nc<0 || nc>=n) return true;
-			// 코어나 전선 만남
-			if(map[nr][nc] != 0) return false;
-		}
-	}
-	// 전선 놓거나 지우기
-	public static int setwire(int r, int c, int d, int value) {
-		int len = 0;
-		int nr = r;
-		int nc = c;
-		while(true) {
-			nr+=dr[d];
-			nc+=dc[d];
-			if(nr<0 || nr>=n || nc<0 || nc>=n) break;
-			map[nr][nc] = value;
-			len++;
-		}
-		return len;
-	}
+    public static class pair{
+        int x, y;
+        public pair(int x, int y){
+            this.x = x ;
+            this.y = y;
+        }
+    }
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    static int[][] map;
+    static List<pair> core;
+    static int n, maxnum, totallen;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for(int test = 1; test<=t; test++){
+            n = Integer.parseInt(br.readLine());
+            map = new int[n][n];
+            core = new ArrayList<>();
+            for(int i=0; i<n;i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for(int j =0; j<n; j++){
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    if(i==0 || i==n-1 || j==0 || j==n-1) continue;
+                    if(map[i][j] == 1) core.add(new pair(i, j));
+                }
+            }
+            maxnum = 0;
+            totallen = Integer.MAX_VALUE;
+            dfs(0,0,0);
+            System.out.println("#"+test+" "+totallen);
+        }
+    }
+    public static void dfs(int cnt, int minlen, int index){
+        if(index == core.size()){
+            if(maxnum<cnt){
+                maxnum = cnt;
+                totallen = minlen;
+            }else if(maxnum == cnt){
+                totallen = Math.min(totallen, minlen);
+            }
+            return;
+        }
+        pair cur = core.get(index);
+        for(int d=0; d<4; d++){
+            if(chkconnect(cur.x, cur.y, d)){
+                int len = setwire(cur.x, cur.y, d, 2);
+                dfs(cnt+1, minlen+len, index+1);
+                setwire(cur.x, cur.y, d, 0);
+            }
+        }
+        dfs(cnt, minlen, index+1);
+    }
+    // 연결가능한지 확인하기
+    private static boolean chkconnect(int a, int b, int d){
+        while(true){
+            a+=dx[d];
+            b+=dy[d];
+            // 순서
+            if(map[a][b]>0) break;
+            if(a==0 || a==n-1 || b==0 || b==n-1) return true;
+
+        }
+        return false;
+    }
+    // 연결시키거나 제거하기
+    private static int setwire(int a, int b, int d, int num){
+        int count = 0;
+        while(true){
+            a+=dx[d];
+            b+=dy[d];
+            if(a<0 || a>=n || b<0 || b>=n) return count;
+            map[a][b] = num;
+            count++;
+        }
+    }
 }
