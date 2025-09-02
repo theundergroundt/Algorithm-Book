fe29db7b7c7118d295fe021882dee2b66bdb064e
sangji <139340193+theundergroundt@users.noreply.github.com>
2025-09-02 14:36:33 +0900

    [Gold III] Title: 줄 세우기, Time: 812 ms, Memory: 57828 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -8,36 +8,36 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		int[] dig = new int[n+1];
-		List<Integer>[] li = new ArrayList[n+1];
-		List<Integer> result = new ArrayList<>();
-		for(int i=0; i<=n; i++) li[i] = new ArrayList<>();
+		List<Integer> li[] = new ArrayList[n+1];
+		for(int i=1; i<=n; i++) li[i] = new ArrayList<>();
 		for(int i=0; i<m; i++) {
-			StringTokenizer st1 = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st1.nextToken());
-			int b = Integer.parseInt(st1.nextToken());
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			li[a].add(b);
 			dig[b]++;
 		}
 		Queue<Integer> q = new LinkedList<>();
 		for(int i=1; i<=n; i++) {
-			if(dig[i] == 0) q.add(i);
+			if(dig[i]==0)q.add(i);
 		}
+		List<Integer> result = new ArrayList<>();
 		while(!q.isEmpty()) {
 			int cur = q.poll();
 			result.add(cur);
 			for(int c : li[cur]) {
 				dig[c]--;
-				if(dig[c] == 0) q.add(c);
+				if(dig[c]==0) {
+					q.add(c);
+				}				
 			}
 		}
-		for(int c : result) {
-			System.out.print(c+" ");
-		}
+		for(int a : result) System.out.print(a+" ");
 	}
 }
