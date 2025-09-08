e446b1da248198d7a3e7ee7f42756e3b193e44c3
sangji <139340193+theundergroundt@users.noreply.github.com>
2025-09-08 11:53:06 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -6,24 +6,26 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+		long total = 0;
 		int[] arr = new int[n+1];
-		long[][] dp = new long[n+1][3];
+		int[] d = new int[n+1];
 		for(int i=1; i<=n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			total += arr[i];
 		}
-		if(n == 1) {
-			System.out.println(arr[1]);
+		if(n<=2) {
+			System.out.println(total);
 			return;
 		}
-		dp[1][1] = arr[1];
-		dp[1][2] = 0;
-		dp[2][1] = arr[2];
-		dp[2][2] = arr[1]+arr[2];
-		for(int i=3; i<=n; i++) {
-			dp[i][1] = Math.max(dp[i-2][1], dp[i-2][2]) + arr[i];
-			dp[i][2] = dp[i-1][1] + arr[i];			
+		// d[1] 최솟값 : d[1]
+		// d[2] 최솟값 : d[2]
+		d[1] = arr[1];
+		d[2] = arr[2];
+		d[3] = arr[3];
+		for(int i=4; i<n; i++) {
+			d[i] = arr[i]+Math.min(d[i-2], d[i-3]);
 		}
-		long result = Math.max(dp[n][1], dp[n][2]);
-		System.out.println(result);
+		int result = Math.min(d[n-1], d[n-2]);
+		System.out.println(total - result);
 	}
 }
