64f1c23241f1b405dd1c6c3c93eb9feab25700b8
sangji <139340193+theundergroundt@users.noreply.github.com>
2026-07-04 14:41:36 +0900

    [level 2] Title: 배달, Time: 3.56 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 class Solution {
-    
     static class Node implements Comparable<Node>{
         int index;
         int cost;
@@ -17,55 +16,48 @@ class Solution {
     }
     
     public int solution(int N, int[][] road, int K) {
-        
-        List<List<Node>> graph = new ArrayList<>();
+        // 인접리스트 만들기
+        List<List<Node>> li = new ArrayList<>();
         for(int i=0; i<=N; i++){
-            graph.add(new ArrayList<>());
+            li.add(new ArrayList<>());
         }
-        
-        // 그래프 값 저장
+        // 인접리스트 채우기
         for(int[] r : road){
-            int u = r[0];
-            int v = r[1];
-            int cost = r[2];
-            
-            // 양방향 통행
-            graph.get(u).add(new Node(v, cost));
-            graph.get(v).add(new Node(u, cost));            
+            int a = r[0];
+            int b = r[1];
+            int c = r[2];
+            li.get(a).add(new Node(b,c));
+            li.get(b).add(new Node(a,c));
+        }
+        // 우선순위 큐 준비
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(1, 0));
+        
+        int[] dist = new int[N+1];
+        for(int i=0; i<=N; i++){
+            dist[i] = Integer.MAX_VALUE;
         }
         
-        // dist max로 초기화
-        int[] dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        
-        // 출발지
         dist[1] = 0;
         
-        // 우선순위 큐 선언 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        
-        // 시작 노드, 비용 = 0 부터 
-        pq.offer(new Node(1, 0));
-        
+        // 다익스트라 
         while(!pq.isEmpty()){
             Node cur = pq.poll();
-            int curIndex = cur.index;
+            int curNode = cur.index;
             int curCost = cur.cost;
             
-            for(Node neighbor : graph.get(curIndex)){
-                int newCost = neighbor.cost + dist[curIndex];
+            for(Node neigh : li.get(curNode)){
+                int newCost = neigh.cost + dist[curNode];
                 
-                if(newCost < dist[neighbor.index]){
-                    dist[neighbor.index] = newCost;
-                    pq.offer(new Node(neighbor.index, newCost));
+                if(dist[neigh.index] > newCost){
+                    dist[neigh.index] = newCost;
+                    pq.offer(new Node(neigh.index, newCost));
                 }
             }
         }
         int answer = 0;
         for(int i=1; i<=N; i++){
-            if(dist[i] <= K){
-                answer++;
-            }
+            if(dist[i]<=K) answer++;
         }
         return answer;
     }
