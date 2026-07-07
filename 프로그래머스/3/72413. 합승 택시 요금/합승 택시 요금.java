76cc30456aa34febbeea433042c96038a682f33c
sangji <139340193+theundergroundt@users.noreply.github.com>
2026-07-07 21:52:43 +0900

    [level 3] Title: 합승 택시 요금, Time: 39.53 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -2,37 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = 0;
-        
-        
         int[][] dist = new int[n+1][n+1];
-        for(int i=0; i<=n; i++){
+        for(int i=1; i<=n; i++){
             Arrays.fill(dist[i], 100000000);
             dist[i][i] = 0;
         }
-        
-        
         for(int[] f : fares){
-            int A = f[0];
-            int B = f[1];
-            int C = f[2];
-            dist[A][B] = C;
-            dist[B][A] = C;
+            int c = f[0];
+            int d = f[1];
+            int e = f[2];
+            dist[c][d] = e;
+            dist[d][c] = e;
         }
         
-        
+        // 플로이드-워샬
         for(int k = 1; k<=n; k++){
-            for(int i=1; i<=n; i++){
-                for(int j=1; j<=n; j++){
+            for(int i = 1; i<=n; i++){
+                for(int j = 1; j<=n; j++){
                     if(dist[i][j] > dist[i][k] + dist[k][j]){
                         dist[i][j] = dist[i][k] + dist[k][j];
                     }
                 }
             }
         }
-        answer = 987654321;
+        int answer = 987654321;
         for(int i=1; i<=n; i++){
-            answer = Math.min(dist[a][i] + dist[i][b] + dist[s][i], answer);
+            answer = Math.min(answer, dist[s][i] + dist[i][a] + dist[i][b]);
         }
         return answer;
     }
