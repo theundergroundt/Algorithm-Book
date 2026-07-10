e9fadebef13010b9a58712992410ffa016217011
sangji <139340193+theundergroundt@users.noreply.github.com>
2026-07-10 16:45:48 +0900

    [level 3] Title: 가장 먼 노드, Time: 16.38 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,7 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        // 가중치 = 1 + 시작점 고정 -> bfs
+        
+        // 시작점 1로 고정 bfs
+        
         List<List<Integer>> graph = new ArrayList<>();
         for(int i=0; i<=n; i++) graph.add(new ArrayList<>());
         
@@ -13,24 +15,26 @@ class Solution {
         }
         
         Queue<Integer> q = new LinkedList<>();
-        q.offer(1);
+        q.add(1);
         
         int[] dist = new int[n+1];
         Arrays.fill(dist, -1);
         dist[1] = 0;
         
         int maxnum = 0;
+        
         while(!q.isEmpty()){
             int cur = q.poll();
-            for(int c : graph.get(cur)){
-                if(dist[c] != -1) continue;
-                dist[c] = dist[cur] + 1;
-                q.offer(c);
-                maxnum = Math.max(maxnum, dist[c]);
+            
+            for(int g : graph.get(cur)){
+                if(dist[g] != -1) continue;
+                dist[g] = dist[cur]+1;
+                q.offer(g);
+                maxnum = Math.max(dist[g], maxnum);
             }
         }
-        for(int a : dist){
-            if(a == maxnum) answer++;
+        for(int i=1; i<=n; i++){
+            if(dist[i] == maxnum) answer++;
         }
         
         return answer;
