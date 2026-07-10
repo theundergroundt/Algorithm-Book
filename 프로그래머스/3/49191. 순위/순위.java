5c902110f99ea9cd7257227aacdfa20547e9060b
sangji <139340193+theundergroundt@users.noreply.github.com>
2026-07-10 16:10:14 +0900

    [level 3] Title: 순위, Time: 5.61 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -2,36 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] results) {
-        int answer = 0;
-        // 2 3 4 
-        // 2 (1) 3 4
-        // 5 2 
-        
-        boolean[][] dist = new boolean[n+1][n+1];
+        int answer=0;
+        boolean[][] graph = new boolean[n+1][n+1];
         
         for(int[] r : results){
-            dist[r[0]][r[1]] = true;
+            graph[r[0]][r[1]] = true;
         }
         
         for(int k=1; k<=n; k++){
             for(int i=1; i<=n; i++){
                 for(int j=1; j<=n; j++){
-                    if(dist[i][k] && dist[k][j]) dist[i][j] = true;
+                    if(graph[i][k] && graph[k][j]) graph[i][j] = true;
                 }
             }
         }
-        
-        // n-1 = 지는 선수 + 이긴 선수 
         for(int i=1; i<=n; i++){
             int winnum = 0;
             int defeatnum = 0;
             for(int j=1; j<=n; j++){
-                if(dist[i][j]) winnum++;
-                if(dist[j][i]) defeatnum++;
+                if(graph[i][j]) winnum++;
+                if(graph[j][i]) defeatnum++;
             }
-            if(winnum + defeatnum == n-1) answer++;
+            if(winnum+defeatnum == n-1) answer++;
         }
-        
         return answer;
     }
 }
