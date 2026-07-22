e0990053074b8bda61184e6e936071bceeb38d16
sangji <139340193+theundergroundt@users.noreply.github.com>
2026-07-22 13:28:41 +0900

    [level 3] Title: 에어컨, Time: 2.61 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/3/214289. 에어컨/에어컨.java
+++ 프로그래머스/3/214289. 에어컨/에어컨.java
@@ -1,13 +1,13 @@
 import java.util.*;
+
 class Solution {
-    static int[][] dp;
-    static int[] onboard;
-    static int temperature, t1, t2, a, b;
     
-    static final int OFFSET = 10;
+    static int temperature, t1, t2, a, b, answer;
+    static int[] onboard;
+    static int[][] dp;
+    
     public int solution(int temperature, int t1, int t2, int a, int b, int[] onboard) {
-        int answer = 0;
-        
+        answer = 0;
         this.temperature = temperature;
         this.t1 = t1;
         this.t2 = t2;
@@ -15,43 +15,45 @@ class Solution {
         this.b = b;
         this.onboard = onboard;
         
-        dp = new int[onboard.length + 1][100];
-        for(int i=0; i< dp.length; i++){
+        dp = new int[onboard.length+1][51];
+        
+        for(int i=0; i<dp.length; i++){
             Arrays.fill(dp[i], -1);
         }
+        
         answer = dfs(0, temperature);
+        
         return answer;
     }
-    static public int dfs(int t, int temp){
-        if(t == onboard.length){
-            return 0;
-        }
+    
+    static int dfs(int t, int temp){
+        if(t == onboard.length) return 0;
         
         if(temp<-10 || temp>40) return 100000000;
         
-        // 승객이 타고 있는데 쾌적 온도 외 인경우 안 고르게
         if(onboard[t] == 1){
-            if(temp<t1 || temp>t2) return 100000;
+            if(temp<t1 || temp>t2) return 100000000;
         }
         
-        // 이미 계산한값이면 바로 리턴
-        if(dp[t][temp + OFFSET] != -1) return dp[t][temp + OFFSET];
+        if(dp[t][temp + 10] != -1) return dp[t][temp + 10];
         
-        int besttemp = 100000000;
+        // 에어컨 킨 상태
+        // 온도올리기
+        // 온도내리기
+        // 온도 유지하기
         
-        besttemp= Math.min(besttemp, a + dfs(t+1, temp+1));
-        besttemp= Math.min(besttemp, a + dfs(t+1, temp-1));
-        besttemp= Math.min(besttemp, b + dfs(t+1, temp));
+        int minset = 987654321;
+        minset = Math.min(dfs(t+1, temp+1) + a, minset);
+        minset = Math.min(dfs(t+1, temp-1) + a, minset);
+        minset = Math.min(dfs(t+1, temp) + b, minset);
             
-        // 에어컨 off
-        int offtemp;
-        if(temp < temperature) offtemp = temp+1;
-        else if(temp > temperature) offtemp = temp-1;
-        else offtemp = temp;
-        besttemp = Math.min(besttemp, 0 + dfs(t+1, offtemp));
-            
-        dp[t][temp + OFFSET] = besttemp;
-        return besttemp;
+        // 에어컨 끈 상태
+        // 온도 올라가기 or 온도 내려가기 or 온도 같아서 유지
+        if(temp < temperature) minset = Math.min(dfs(t+1, temp+1), minset);
+        else if(temp > temperature) minset = Math.min(dfs(t+1, temp-1), minset);
+        else minset = Math.min(dfs(t+1, temp), minset);
         
+        dp[t][temp + 10] = minset;
+        return minset;        
     }
 }
